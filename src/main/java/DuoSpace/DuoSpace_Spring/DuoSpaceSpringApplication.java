7444eafe43baad58a5f75bcaa89314b910c7de96
Npvpr <nlmnever2@gmail.com>
2024-05-30 17:08:25 +0700

    First Setup

---
+++ src/main/java/DuoSpace/DuoSpace_Spring/DuoSpaceSpringApplication.java
@@ -0,0 +1,13 @@
+package DuoSpace.DuoSpace_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoSpaceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuoSpaceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DuoSpace/DuoSpace_Spring/DuoSpaceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package DuoSpace.DuoSpace_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoSpaceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
